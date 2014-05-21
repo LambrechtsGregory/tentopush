69600238c5728024661644af3066bb68516891ad
Gregory Lambrechts <lambrechts_gregory@hotmail.com>
2014-05-21 09:08:03 +0200

    first setup

--- lib/tentopush.rb
+++ lib/tentopush.rb
@@ -1,15 +1,18 @@
-require "tentopush/version"
-
+require 'net/http'
+require 'json'
+require 'uri'
 class Tentopush
   
-  def initialize()
+  def initialize(package_name, secret_key)
     @destination = URI('http://192.168.1.7:3000/api/pushes')
-    # secret_key en package_name hier init.
+    @package_name = package_name
+    @secret_key = secret_key
   end
   
   #overload + unique apid (of meerdere).
-  def send_push(package_name, secret_key, message)
-    res = Net::HTTP.post_form(@destination, 'package_name' => package_name, 'secret_key' => secret_key, 'message' => message)
+  def send_push(message)
+    Net::HTTP.post_form URI(@destination),
+                   { 'package_name' => @package_name, 'secret_key' => @secret_key, 'message' => message }
   end  
   
 end
--- lib/tentopush/version.rb
+++ lib/tentopush/version.rb
@@ -1,3 +1,3 @@
-module Tentopush
+class Tentopush
   VERSION = "0.0.1"
 end
